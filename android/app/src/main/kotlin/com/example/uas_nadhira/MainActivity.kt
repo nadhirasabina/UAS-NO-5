ba2341d13dce81b4707f2b88f4e767420af1d32a
nadhirasabina <nadirasabinaput@gmail.com>
2024-01-18 21:50:08 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/uas_nadhira/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.uas_nadhira
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:uas_nadhira/models/person.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'screens/info_screens.dart';
+
+main() async {
+// Initialize hive
+  await Hive.initFlutter();
+// Registering the adapter
+  Hive.registerAdapter(PersonAdapter());
+// Opening the box
+  await Hive.openBox('peopleBox');
+  runApp(MyApp());
+}
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+class _MyAppState extends State<MyApp> {
+  @override
+  void dispose() {
+// Closes all Hive boxes
+    Hive.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hive Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: InfoScreen(),
+    );
+  }
+}
